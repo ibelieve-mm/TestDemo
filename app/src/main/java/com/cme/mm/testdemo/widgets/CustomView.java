78936f7d7997a076dc95a1c7bf0e5059a70ffa33
ibelieve-mm <ibelieve1210@163.co>
2016-09-12 17:36:53 +0800

    实现一个CustomView类，对自定义View中的方法做一个基本了解

--- app/src/main/java/com/cme/mm/testdemo/widgets/CustomView.java
+++ app/src/main/java/com/cme/mm/testdemo/widgets/CustomView.java
@@ -1,7 +1,93 @@
 package com.cme.mm.testdemo.widgets;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
 /**
  * Created by Administrator on 2016/9/12.
  */
-public class CustomView {
+public class CustomView extends View {
+
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     *回调该方法来进行测量
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(measureSpec(widthMeasureSpec,100), measureSpec(heightMeasureSpec, 50));
+    }
+
+    /**
+     * 从XML加载组件后回调
+     */
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+
+    /**
+     * 组件大小改变时回调
+     * @param w
+     * @param h
+     * @param oldw
+     * @param oldh
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    /**
+     * 回调该方法来确定显示的位置
+     * @param changed
+     * @param left
+     * @param top
+     * @param right
+     * @param bottom
+     */
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    /**
+     * 监听到触摸事件时回调
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    private int measureSpec(int measureSpec ,int def) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY) {//精确模式，即为宽、高指定具体数值，如100dp，或者是match_parent
+            result = specSize;
+        } else {
+            result = def;//当模式为其他两种时，给一个默认的大小
+            if (specMode == MeasureSpec.AT_MOST) {//最大值模式，即为宽、高指定wrap_content
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
+    }
 }
---
+++ app/src/main/java/com/cme/mm/testdemo/widgets/XTextView.java
@@ -0,0 +1,28 @@
+package com.cme.mm.testdemo.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2016/9/12.
+ */
+public class XTextView extends TextView {
+    public XTextView(Context context) {
+        super(context);
+    }
+
+    public XTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public XTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        
+    }
+}
