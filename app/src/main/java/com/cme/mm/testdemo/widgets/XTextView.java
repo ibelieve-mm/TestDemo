f2ba261afafb89bbd0a7366c6cfe33674f206e96
ibelieve-mm <ibelieve1210@163.com>
2016-09-12 22:15:02 +0800

    添加两个TextView的扩展自定义类，XTextView和XTextView1

--- app/src/main/java/com/cme/mm/testdemo/widgets/XTextView.java
+++ app/src/main/java/com/cme/mm/testdemo/widgets/XTextView.java
@@ -1,10 +1,14 @@
 package com.cme.mm.testdemo.widgets;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
 /**
+ * 为TextView添加两个矩形背景
  * Created by Administrator on 2016/9/12.
  */
 public class XTextView extends TextView {
@@ -21,8 +25,24 @@ public class XTextView extends TextView {
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        
+    protected void onDraw(Canvas canvas) {
+        //在系统绘制文字之前先绘制两个矩形
+        Paint mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.FILL);
+
+        mPaint.setColor(Color.MAGENTA);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);//绘制内边框
+
+        mPaint.setColor(Color.LTGRAY);
+        canvas.drawRect(10, 10, getMeasuredWidth() - 10, getMeasuredHeight() - 10, mPaint);//绘制内边框
+
+        canvas.save();//对画布进行操作前,要保存状态
+
+        //绘制文字前平移10像素
+//        canvas.translate(10,0);
+
+        super.onDraw(canvas);
+
+        canvas.restore();//最后恢复画布
     }
 }
---
+++ app/src/main/java/com/cme/mm/testdemo/widgets/XTextView1.java
@@ -0,0 +1,63 @@
+package com.cme.mm.testdemo.widgets;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2016/9/12.
+ */
+public class XTextView1 extends TextView {
+    public XTextView1(Context context) {
+        super(context);
+    }
+
+    public XTextView1(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public XTextView1(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private float mViewWidth;
+    private float mTranslate;
+    private Paint mPaint;
+    private LinearGradient mLinearGradient;//线性梯度
+    private Matrix mGradientMatrix;//矩阵
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        //该段代码中最重要的是使用getPaint()获取当前绘制TextView的Paint对象,并且给这个Paint对象设置原生TextView没有的LinearGradient属性
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0, new int[]{Color.BLUE, 0xffffffff, Color.BLUE}, null, Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (null != mGradientMatrix) {
+            mTranslate += mViewWidth / 10;
+            if (mTranslate >  mViewWidth) {
+                mTranslate = -mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate, 0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(100);
+        }
+    }
+}
