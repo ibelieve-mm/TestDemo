56d525847794a054b6cc699242c3e156f61344c4
ibelieve-mm <ibelieve1210@163.co>
2016-09-20 08:52:45 +0800

    在CustomVIewTestActivity类中对Android坐标系和视图坐标系的区别

--- app/src/main/java/com/cme/mm/testdemo/activity/CustomScrollViewTestActivity.java
+++ app/src/main/java/com/cme/mm/testdemo/activity/CustomScrollViewTestActivity.java
@@ -13,7 +13,6 @@ import org.androidannotations.annotations.EActivity;
  * Date：2016/9/18
  * Email：ibelieve1210@163.com
  */
-
 @EActivity(R.layout.activity_custom_scroll_view_test)
 public class CustomScrollViewTestActivity extends AppCompatActivity {
 
--- app/src/main/java/com/cme/mm/testdemo/activity/CustomViewTestActivity.java
+++ app/src/main/java/com/cme/mm/testdemo/activity/CustomViewTestActivity.java
@@ -1,10 +1,13 @@
 package com.cme.mm.testdemo.activity;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 import com.cme.mm.testdemo.App;
 import com.cme.mm.testdemo.R;
 import com.cme.mm.testdemo.widgets.complex.TopBar;
+import com.cme.mm.testdemo.widgets.text_view.XTextView;
+import com.cme.mm.testdemo.widgets.text_view.XTextView1;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -22,10 +25,36 @@ public class CustomViewTestActivity extends AppCompatActivity {
 
     @ViewById(R.id.topBar)
     TopBar topBar;
+    @ViewById(R.id.xtv1)
+    XTextView1 xtv1;
+    @ViewById(R.id.view)
+    XTextView view;
 
     @AfterViews
     void onPageStart() {
         setListener();
+
+        int[] xtv1Location = new int[2];
+        xtv1.getLocationOnScreen(xtv1Location);//获取整个屏幕的绝对坐标
+
+        int[] xtvLocation = new int[2];
+        view.getLocationInWindow(xtvLocation);//获取当前窗口的绝对坐标
+
+        App.log("xtv1 Location：(" + xtv1Location[0] + "," + xtv1Location[1] + ")");
+        App.log("view Location：(" + xtvLocation[0] + "," + xtvLocation[1] + ")");
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        App.log("event RawX/Y "+  event.getRawX()+ "," +event.getRawY());//获取到的结果是触摸位置的在Android坐标系中的绝对位置
+        App.log("event X/Y "+  event.getX()+ "," +event.getY());//获取到的结果是触摸位置的在Android坐标系中的绝对位置
+
+        App.log("View X/Y "+  view.getX() + "," +view.getY());//获取到View左上角顶点距父控件左边/上边的距离
+        App.log("View Left/Right/Top/Bottom "
+                +  view.getLeft() + "," +view.getRight() //获取到View自身左边/右边距离父控件左边的距离，所以他俩的差值即为控件自身的宽度
+                + "," +view.getTop() + "," +view.getBottom());//获取到View自身上边/下边距离父控件上边的距离，所以他俩的差值即为控件自身的宽度
+        App.log("View Width/Height "+  view.getWidth() + "," +view.getHeight());//获取控件自身的宽度/高度
+        return super.onTouchEvent(event);
     }
 
     private void setListener() {
@@ -40,5 +69,6 @@ public class CustomViewTestActivity extends AppCompatActivity {
                 App.toast("右边的按钮被点击了~~");
             }
         });
+
     }
 }
